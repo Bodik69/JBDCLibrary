03a5744e98203dcb67de5dff1aba34ad89f08e8e
Bodik69 <bsmachylo86@gmail.com>
2015-12-08 16:04:19 +0200

    add first query

--- src/main/java/com/softserveinc/App.java
+++ src/main/java/com/softserveinc/App.java
@@ -4,15 +4,15 @@ import java.sql.*;
 
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws SQLException {
         //Ihor's methods
 
 
         //Bohdan's methods
-
+        ReadersDAO.getListOfDebtors();
 
         //Ruslan's methods
 
+        JDBConnector.getInstance().close();
     }
 }
---
+++ src/main/java/com/softserveinc/ReadersDAO.java
@@ -0,0 +1,32 @@
+package com.softserveinc;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class ReadersDAO {
+
+    /**
+     * print readers who did not return books on time
+     */
+    public static void getListOfDebtors() {
+        try (Statement statement = JDBConnector.getInstance().createStatement()) {
+            ResultSet result = statement.executeQuery("SELECT * FROM reader where idReader IN" +
+                    " (SELECT idReader FROM orderreader WHERE" +
+                    " (dataReturn IS NULL) && DATEDIFF(CURRENT_TIMESTAMP, dataOrder) > 31);");
+            while(result.next()) {
+                String name = result.getString("name");
+                String surname = result.getString("surname");
+                String address = result.getString("address");
+                String phone = result.getString("phone");
+                String birth = result.getString("birth");
+                String dateOfCreate = result.getString("dateOfCreate");
+                System.out.printf("Name: %s, \t Surname: %s, \t Address: %s, \t Phone: %s, \t Birth: %s", name, surname, address, phone, birth);
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}

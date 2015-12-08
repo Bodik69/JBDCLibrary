5e5a32f6c54ec6f08ff1dc6f7adce00a676f8bee
Bodik69 <bsmachylo86@gmail.com>
2015-12-08 11:47:06 +0200

    Add project

---
+++ Library/src/main/java/com/softserveinc/App.java
@@ -0,0 +1,71 @@
+package com.softserveinc;
+
+import java.sql.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://sql4.freemysqlhosting.net:3306/sql499102";
+
+    //  Database credentials
+    static final String USER = "sql499102";
+    static final String PASS = "zbAu6z2Dlj";
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try{
+            //STEP 2: Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //STEP 3: Open a connection
+            System.out.println("Connecting to database...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            //STEP 4: Execute a query
+            System.out.println("Creating statement...");
+            stmt = conn.createStatement();
+            String sql;
+            sql = "SELECT * FROM Reader";
+            ResultSet rs = stmt.executeQuery(sql);
+
+            //STEP 5: Extract data from result set
+            while(rs.next()){
+                //Retrieve by column name
+                String first = rs.getString("name");
+                String last = rs.getString("surname");
+
+                System.out.print(", First: " + first);
+                System.out.println(", Last: " + last);
+            }
+            //STEP 6: Clean-up environment
+            rs.close();
+            stmt.close();
+            conn.close();
+        }catch(SQLException se){
+            //Handle errors for JDBC
+            se.printStackTrace();
+        }catch(Exception e){
+            //Handle errors for Class.forName
+            e.printStackTrace();
+        }finally{
+            //finally block used to close resources
+            try{
+                if(stmt!=null)
+                    stmt.close();
+            }catch(SQLException se2){
+            }// nothing we can do
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }//end finally try
+        }//end try
+        System.out.println("Goodbye!");
+    }//end main
+}
